9367b69c584f7e91b9669ded4522898f60ac50ea
dudaordahy <dudasciortino@gmail.com>
2025-11-21 22:57:40 -0300

    atualizacao

--- index.php
+++ index.php
@@ -5,15 +5,50 @@ include_once './includes/header.php';
 ?>
 <div class="container_escolhas">
     <form action="./actions/perfil-salvar.php" method="post" enctype="multipart/form-data">
-        <h1>Escolha foto de perfil</h1>
+        <h1>Escolha sua foto de perfil</h1>
         <span class="btn" onclick="document.getElementById('fileInput').click();">Escolher imagem</span>
         <input type="file" name="fotoPerfil" id="fileInput">
         <div id="preview"></div>
-        <button id="proximo" type="submit"></button>
+        <button id="proximo" type="submit"><img src="./assets/img/proximo.png" alt=""></button>
     </form>
-
-<!-- <ul id="listaPreferencias"></ul> -->
 </div>
+
+<div class="nav_bar_inline">
+    <div id="cidade">
+        <?php
+        $UsuarioID = $_SESSION['Usuario']['UsuarioID'];
+
+        // Query correta
+        $sql = "SELECT Cidade FROM usuarios WHERE UsuarioID = $UsuarioID";
+
+        // Executa
+        $result = mysqli_query($conexao, $sql);
+
+        // Pega a linha
+        $row = mysqli_fetch_assoc($result);
+
+        // Pega o valor da cidade
+        $cidade = $row['Cidade'];
+
+        // Exibe
+        echo "<h1 id='txt_cidade'>$cidade</h1>";
+        ?>
+    </div>
+    <div id="perfil">
+        <a href="#" id="btn_perfil"><img src="#" alt=""></a>
+    </div>
+</div>
+
+<div class="nav_bar_coluna">
+    <div id="direct">
+        <a href="#"><img src="#" alt=""></a>
+    </div>
+    <div id="config">
+        <a href="#"><img src="#" alt=""></a>
+    </div>
+</div>
+<!-- <ul id="listaPreferencias"></ul> -->
+
 
 <?php
 include_once './includes/footer.php';
